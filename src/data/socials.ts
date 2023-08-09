1249bcfdc5c3638dec1e726b8317a0fb1964b881
Daniel Hill <dhhillaz@gmail.com>
2023-08-09 11:54:32 -0700

    try removing sticks

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,17 +1,22 @@
 export const socials = [
+  // {
+  //   name: 'Twitter',
+  //   url: 'https://twitter.com/littlesticksdev',
+  //   icon: 'mdi:twitter'
+  // },
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: 'mdi:twitter'
+    name: "GitHub",
+    url: "https://github.com/littlesticks",
+    icon: "mdi:github",
   },
   {
-    name: 'GitHub',
-    url: 'https://github.com/littlesticks',
-    icon: 'mdi:github'
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/daniel-hill-1195b713a/",
+    icon: "mdi:linkedin",
   },
-  {
-    name: 'Instagram',
-    url: 'https://instagram.com',
-    icon: 'mdi:instagram'
-  }
-]
+  // {
+  //   name: 'Instagram',
+  //   url: 'https://instagram.com',
+  //   icon: 'mdi:instagram'
+  // }
+];
